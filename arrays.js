4cb1d86d662bb1bdd80b907365f817c6a0b28e3c
SamanthaMcElhinney <mcelhinneysamantha@gmail.com>
2022-10-19 13:25:02 -0700

    Add variables under arrays

--- arrays.js
+++ arrays.js
@@ -1,1 +1,3 @@
-var stores = ["Best Buy","Hallmark"], 
+var cities = ["Seattle", "Los Angeles", "Boston", "New York", "Vegas"];
+var ages = ["32","5", "21", "25", "70"];
+var tests = [true, false, true, false];
